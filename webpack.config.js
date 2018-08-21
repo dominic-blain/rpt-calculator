54eb436a3d4f6a817d9679be6e00232e354d1a55
Dominic Blain <dominic.blain@gmail.com>
2018-08-20 20:39:49 -0400

    Build basic react environment

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const inputPath = path.resolve(__dirname, 'src', 'index.js');
+const outputPath = path.resolve(__dirname, 'public');
+const templatePath = path.resolve(__dirname, 'src', 'index.html');
+
+module.exports = {
+  entry: {
+    main: inputPath
+  },
+  output: {
+    filename: '[name].bundle.[hash].js',
+    path: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [nodeModulesPath],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    // Clears the build folder before each build
+    new CleanWebpackPlugin([outputPath]),
+    // Generates new indexl.html file with correct filename
+    new HtmlWebpackPlugin({
+      template: templatePath,
+      filename: 'index.html'
+    })
+  ]
+};
