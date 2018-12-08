b0dad09735203f8d4982b56e902a9e181f9fdcaa
Dominic Blain <dominic.blain@gmail.com>
2018-12-08 16:28:27 -0500

    Add styles to empty program view component

--- src/components/EmptyProgram/EmptyProgram.js
+++ src/components/EmptyProgram/EmptyProgram.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ActionCreators from '../../actions/ActionCreators';
+import styles from './EmptyProgram.less';
 
 class EmptyProgram extends React.Component {
     constructor(props) {
@@ -14,9 +15,14 @@ class EmptyProgram extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Oups, no program</h1>
-                <button onClick={this.handleCreateButtonClick}>Create program</button>
+            <div className={styles.emptyProgram}>
+                <h1 className={styles.title}>You don't have any programs yet</h1>
+                <p className={styles.description}>Start by creating a new program or choose from one of our template. You can of course completly customize it to fit your specific needs</p>
+                <button
+                    className={styles.buttonNew} 
+                    onClick={this.handleCreateButtonClick}>
+                    Create new program
+                </button>
             </div>
         )
     }
