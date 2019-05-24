4fd888c3fdf6cbd19717629ff8b8da60ec8e5589
Dominic Blain <dominic.blain@gmail.com>
2019-05-24 19:04:40 -0400

    Clean up code

--- src/components/RestCard/RestCard.js
+++ src/components/RestCard/RestCard.js
@@ -6,9 +6,7 @@ class RestCard extends React.Component {
         super(props);
 
         this.state = {
-            time: this.props.time * 60,
-            isInited: false,
-            intervalId: null
+            time: this.props.time * 60
         }
 
         this.updateCountdown = this.updateCountdown.bind(this);
@@ -20,22 +18,18 @@ class RestCard extends React.Component {
 
     updateCountdown() {
         if (this.state.time === 0) {
-            console.log('END: '+ this.props.exercise.name + ' | Set '+ this.props.order +' id: '+ this.updatingCountdown);
             clearInterval(this.updatingCountdown);
             this.setState(prevState => {
                 return {
-                    time: prevState.time,
-                    isInited: true
+                    time: prevState.time
                 }
             });
             this.props.onEndRest(this.updatingCountdown);
         }
         else {
-            console.log('UPDATE: ' + this.props.exercise.name + ' | Set '+ this.props.order);
             this.setState(prevState => {
                 return {
-                    time: prevState.time - 1,
-                    isInited: true
+                    time: prevState.time - 1
                 }
             });
         }
@@ -48,16 +42,9 @@ class RestCard extends React.Component {
         seconds = seconds < 10 ? '0' + seconds : seconds;
         const displayTime = minutes + ':' + seconds;
 
-        
-
         if (!!this.props.isRunning && !this.updatingCountdown) {
             this.updatingCountdown = setInterval(this.updateCountdown, 1000);
-            console.log('INIT: '+ this.props.exercise.name + ' | Set '+ this.props.order  +' id: '+ this.updatingCountdown);
         }
-        // else if (!this.props.isRunning && this.state.isInited) {
-        //     console.log(this.props.exercise.name + ' CLEAR: Set '+ this.props.order);
-        //     clearInterval(this.updateCountdown);
-        // }
 
         return (
             <div className={styles.restCard}>
